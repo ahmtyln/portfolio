4de9dd6a90569e27b2709914065b15eb5d09a1ad
ahmet <ahmtyln.06@gmail.com>
2024-09-20 23:59:42 +0200

    portfolio completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,38 @@
 import "./intro.scss"
+import { init } from "ityped";
+import { useEffect, useRef} from "react";
+
 
 export default function Intro() {
+
+  const textRef = useRef()
+
+  useEffect(() =>{
+    init(textRef.current, {
+      showCursor:true,
+      backDelay:1500,
+      backSpeed:60,
+      strings: ["Developer", "Mern Developer"],
+    });
+  }, []);
+
   return (
     <div className='intro' id="intro">
-      
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm </h2>
+          <h1>Ahmet Yalin</h1>
+          <h3>Full Stack <span ref = {textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -3,7 +3,25 @@ import "./works.scss"
 export default function Works() {
   return (
     <div className='works' id="works">
-      
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Unde molestiae tempora est quos cumque autem dolorem voluptate 
+                  pariatur deserunt animi!</p>
+                  <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
